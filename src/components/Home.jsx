dec9854b9c52c31e30eba227244a95284a261312
Carlos <carlosmca4424@gmail.com>
2021-09-28 02:53:51 -0400

    little change on gradient style

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,25 +5,25 @@ import { makeStyles } from "@material-ui/styles";
 const useStyle = makeStyles((theme) => ({
   root: {
     height: "100vh",
-    backgroundImage: `linear-gradient(rgb(255, 255, 255, 0), #222222), url(${"/home-background-image.jpg"})`,
+    backgroundImage: `linear-gradient(rgb(255, 255, 255, 0), #212121), url(${"/img/home-background-image.jpg"})`,
     backgroundSize: "cover",
   },
   container: {
     display: "flex",
     flexDirection: "column",
     height: "90%",
-    justifyContent: "center"
+    justifyContent: "center",
   },
   header: {
     fontWeight: 700,
     fontSize: "2.5rem",
   },
   subheader: {
-    margin: "1.5em 0px"
+    margin: "1.5em 0px",
   },
   btn: {
-    margin: "20px 0px"
-  }
+    margin: "20px 0px",
+  },
 }));
 
 const Home = () => {
@@ -31,15 +31,21 @@ const Home = () => {
   return (
     <Box className={classes.root}>
       <NavBar />
-        <Container className={classes.container}>
-          <Typography className={classes.header}>Tour around the world</Typography>
-          <Typography className={classes.header}>with the new destination</Typography>
-          <Box className={classes.subheader}>
-            <Typography>We believe im a traveling experience by providing our tour</Typography>
-            <Typography>plan that suits your the best!</Typography>
-          </Box>
-          <Button className={classes.btn}>Explore now</Button>
-        </Container>
+      <Container className={classes.container}>
+        <Typography className={classes.header}>
+          Tour around the world
+        </Typography>
+        <Typography className={classes.header}>
+          with the new destination
+        </Typography>
+        <Box className={classes.subheader}>
+          <Typography>
+            We believe im a traveling experience by providing our tour
+          </Typography>
+          <Typography>plan that suits your the best!</Typography>
+        </Box>
+        <Button className={classes.btn}>Explore now</Button>
+      </Container>
     </Box>
   );
 };
